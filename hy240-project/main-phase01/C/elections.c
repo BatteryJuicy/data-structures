529abb52e52091f92f1c734ec799561f6e99482b
BatteryJuice <andreasth005@gmail.com>
2024-11-17 18:16:38 +0200

    events A, D, S done

--- hy240-project/main-phase01/C/elections.c
+++ hy240-project/main-phase01/C/elections.c
@@ -4,7 +4,46 @@
 
 #include "elections.h"
 
+extern struct district Districts[56];
+extern struct party Parties[5];
+extern struct parliament Parliament;
+
 void announce_elections()
 {
+    for (int i = 0; i < 56; i++)
+    {
+        Districts[i].did = -1;
+        Districts[i].seats = -1;
+        Districts[i].allotted = -1;
+        Districts[i].blanks = -1;
+        Districts[i].voids = -1;
+        Districts[i].stations = NULL;
+        Districts[i].candidates = NULL;
+    }
+    for (int i = 0; i < 5; i++)
+    {
+        Parties[i].pid = -1;
+        Parties[i].nelected = -1;
+        Parties[i].elected = NULL;
+    }
+
+    Parliament.members = NULL;    
+    printf("A\nDONE\n");
+
+}
+
+int create_district(int did, int seats)
+{
+    Districts[did].did = did;
+    Districts[did].seats = seats;
+    Districts[did].allotted = 0;
+
+    printf("D %d %d\n", did, seats);
+    printf("Districts =");
+    for (int i = 0; i < did; i++)
+    {
+        printf(" %d ", did);
+    }
+    printf("\nDONE\n");
     
 }
