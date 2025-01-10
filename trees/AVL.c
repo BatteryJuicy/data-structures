0dca94d7d58c79db341d665101efa087a7765802
BatteryJuice <andreasth005@gmail.com>
2025-01-10 20:00:01 +0200

    finished avl implementation insert. Only delete remains

--- trees/AVL.c
+++ trees/AVL.c
@@ -5,7 +5,6 @@
 typedef struct tree 
 {
     int data;
-    int depth;
     int balance; // values should be: -1, 0, 1. Otherwise the tree needs to be rebalanced.
     struct tree* parent;
     struct tree* lc;
@@ -22,13 +21,6 @@ void print_node(node* node_)
     printf("%d", node_->data);
 }
 
-int isLeaf(node* node_)
-{
-    if (node_->lc == NULL && node_->rc == NULL)
-        return 1;
-    return 0;
-}
-
 int get_height(node* root)
 {
     if (root == NULL)
@@ -39,20 +31,20 @@ int get_height(node* root)
     return (leftHeight > rightHeight ? leftHeight : rightHeight) + 1;
 }
 
-void print_level(node* node, int level)
+void print_level(node* node_, int level)
 {
-    if (node == NULL){
+    if (node_ == NULL){
         print_node(NULL);
         return;
     }
     if (level == 1){
-        print_node(node);
+        print_node(node_);
     }
     else if (level > 1)
     {
-        print_level(node->lc, level - 1);
+        print_level(node_->lc, level - 1);
         putchar(' ');
-        print_level(node->rc, level - 1);
+        print_level(node_->rc, level - 1);
     }
 }
 
@@ -71,31 +63,135 @@ void print_tree(node* root)
     }
 }
 
-node* create_node(int data, int depth)
+node* create_node(int data)
 {
     node *new_node = (node*) malloc(sizeof(node));
     if (new_node == NULL){
-        printf("couldn't allocate node with\n\tdata: %d\n\tdepth: %d\n", data, depth);
+        printf("couldn't allocate node with\n\tdata: %d\n", data);
         exit(1);
     }
     new_node->lc = new_node->rc = new_node->parent = NULL;
     new_node->data = data;
-    new_node->depth = depth;
     new_node->balance = 0;
 
     return new_node;
 }
 
-void calculate_balance(node* node)
+void calculate_balance(node* node_)
 {
     int left_height;
     int right_height;
 
-    left_height = get_height(node->lc);
-    right_height = get_height(node->rc);
+    left_height = get_height(node_->lc);
+    right_height = get_height(node_->rc);
 
-    int balance = right_height - left_height;
-    node->balance = balance;
+    int balance = left_height - right_height;
+    node_->balance = balance;
+}
+
+node* left_rotate(node* x)
+{
+    node* y = x->rc; 
+    node* b = y->lc;
+    /*
+              x
+            /   \ 
+           a     y
+                / \ 
+               b   c
+    */
+
+    //rotate.
+    y->lc = x;
+    x->rc = b;
+
+    if (b != NULL)
+        b->parent = x;
+
+    y->parent = x->parent;
+    x->parent = y;
+
+    calculate_balance(x);
+    calculate_balance(y);
+
+    if (y->parent == NULL)
+        return y;
+
+    if (y->parent->rc == x){
+        y->parent->rc = y;
+    }
+    else if (y->parent->lc == x){
+        y->parent->lc = y;
+    }
+
+    return y;
+}
+
+node* right_rotate(node* y)
+{
+    node* x = y->lc;
+    node* b = x->rc;
+    /*
+              y
+            /   \ 
+           x     c
+          / \ 
+         a   b
+    */
+
+    //rotate.
+    x->rc = y;
+    y->lc = b;
+
+    if (b != NULL)
+        b->parent = x;
+
+    x->parent = y->parent;
+    y->parent = x;
+
+    calculate_balance(x);
+    calculate_balance(y);
+
+    if (x->parent == NULL)
+        return x;
+
+    if (x->parent->rc == y){
+        x->parent->rc = x;
+    }
+    else if (x->parent->lc == y){
+        x->parent->lc = x;
+    }
+
+    return x;
+}
+
+node* balance_tree(node* node_)
+{   
+
+    while(node_ != NULL)
+    {
+        calculate_balance(node_);
+
+        if (node_->balance >= 2){
+            if (node_->lc != NULL && node_->lc->balance < 0){
+                node_->lc = left_rotate(node_->lc);
+            }
+            node_ = right_rotate(node_);
+        }
+        else if (node_->balance <= -2){
+            if (node_->rc != NULL && node_->rc->balance > 0){
+                node_->rc = right_rotate(node_->rc);
+            }
+            node_ = left_rotate(node_);
+        }
+
+        if (node_->parent == NULL){
+            return node_; //return new root.
+        }
+
+        node_ = node_->parent;
+    }
+    return NULL;
 }
 
 void insert(node** root, int k)
@@ -103,10 +199,9 @@ void insert(node** root, int k)
     node* parent = NULL;
     node* q = *root;
 
-    int depth = 0;
-
     while(q != NULL)
     {
+        
         if (k == q->data){
             printf("node with data %d already exists\n", k);
             return;
@@ -116,11 +211,9 @@ void insert(node** root, int k)
             q = q->rc;
         else
             q = q->lc;
-        depth++;
     }
 
-    node* new_node = create_node(k, depth);
-    calculate_balance(new_node);
+    node* new_node = create_node(k);
 
     //if the node is not the root.
     if (parent != NULL)
@@ -135,23 +228,23 @@ void insert(node** root, int k)
     }
     new_node->parent = parent;
 
-    //balance the tree if needed.
-    q = new_node;
-    for (int i = depth; i > 0; i--)
-    {
-        if(q->balance < -1 || q->balance > 1){
-            balance_tree();
-        }
-        q = q->parent;
-    }
-    
+    //balance the tree.
+    (*root) = balance_tree(new_node); //the only case where the root value will change is if function performs a rotation on the root.
+
+    print_tree(*root);
+    printf("\n----------\n");
 }
 
-int main(int argc, char** argv)
+int main()
 {
     node* root = NULL;
 
-    insert(&root, 30);
+    insert(&root, 1);
+    insert(&root, 2);
+    insert(&root, 3);
+    insert(&root, 10);
+    insert(&root, 5);
+    insert(&root, 7);
+    insert(&root, 8);
 
-    print_tree(root);
 }
